4ff5f40ee04c8ebaab5e297a29be2e9431b6202b
Rishabh <61441648+rishbobb@users.noreply.github.com>
2023-08-30 14:09:35 -0700

    Update flash.sh

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,2 +1,2 @@
-arduino-cli compile --fqbn arduino:avr:uno ../arduino
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno ../arduino
+~/bin/arduino-cli compile --fqbn arduino:avr:uno ../arduino
+~/bin/arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno ../arduino
