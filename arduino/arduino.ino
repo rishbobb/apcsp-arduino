8a0b3daf6ae8ce9755ef48659be847fcb7676ec7
Rishabh <61441648+rishbobb@users.noreply.github.com>
2023-08-30 17:34:34 -0700

    add dynamic serial comm

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,59 +1,99 @@
-#include "LiquidCrystal.h"
+#include "include/LiquidCrystal.h"
 
 #define BUTTON_PIN 3
 
 LiquidCrystal lcd(13,12,8,7,6,5);
 
 int counter = 0;
+int ticks = 0;
 
-void RGBcolor(int red, int green, int blue) {
+void setColor(int red, int green, int blue) {
     analogWrite(11, red);
     analogWrite(10, green);
     analogWrite(9, blue);
+}
+
+void initializeLCDPins() {
+    pinMode(11, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(9, OUTPUT);
+}
+
+void printLCD(String text) {
+    lcd.clear()
+    lcd.setCursor(0,0);
+    lcd.print(text);
+}
+
+void wirelessWrite() {
+    if (Serial.available() > 0) {
+        printLCD(Serial.readln())
+    }
 }
 
 void setup() {
     lcd.begin(16,2);
     lcd.clear();
 
-    pinMode(11, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(9, OUTPUT);
+    initializeLCDPins();
+
     Serial.begin(9600);
     pinMode(BUTTON_PIN, INPUT);
+
+    while (!Serial) {
+        ; // wait for serial
+    }
+}
+
+void imrieRequirements() {
+    if (digitalRead(BUTTON_PIN) == LOW) {
+        RGBcolor(0,0,0);
+        counter++;
+    }
+
+    // printLCD(counter)
+
+    switch(counter) {
+        case 0:
+            setColor(0,0,0);
+            // printLCD("off")
+            break;
+        case 1:
+            setColor(148, 0, 211);
+            break;
+        case 2:
+            setColor(75, 0, 130);
+            break;
+        case 3:
+            setColor(0, 0, 255);
+            break;
+        case 4:
+            setColor(0, 255, 0);
+            break;
+        case 5:
+            setColor(255, 255, 0);
+            break;
+        case 6:
+            setColor(255, 127, 0);
+            break;
+        case 7:
+            setColor(255, 0 , 0);
+            break;
+        case 8:
+            counter = 0;
+            break;
+        default:
+            counter = 0;
+            break;
+    }
 }
 
 void loop() {
-    delay(20);
-    lcd.setCursor(0,0);
-    lcd.print(counter);
-    Serial.println(counter);
-    int buttonState;
-    buttonState = digitalRead(BUTTON_PIN);
-    Serial.println(buttonState);
-    if (buttonState == LOW) {
-        RGBcolor(0,0,0);
-        counter++;
-        lcd.clear();
+    if (ticks % 20 == 0) {
+        imrieRequirements();
     }
-    if (counter == 0) {
-        RGBcolor(0,0,0);
-        lcd.setCursor(0,0);
-        lcd.clear();
-        lcd.print("Off");
-    } else if (counter == 1) {
-        RGBcolor(235,52,195);
-    } else if (counter == 2) {
-        RGBcolor(27, 165, 207);
-    } else if (counter == 3){
-        RGBcolor (150, 75, 29);
-    } else if(counter == 4){
-        RGBcolor (122, 29, 150);
-    } else if(counter == 5){
-        RGBcolor (180, 85, 17);
-    } else if(counter == 6){
-        counter = 0;
-    } else {
-        counter = 0;
-    }
+
+    wirelessWrite();
+    
+    ticks++;
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { execSync } from "child_process";
+import { SerialPort } from "serialport";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+const arduino = new SerialPort({
+  path: "/dev/ttyUSB0",
+  baudRate: 9600,
+});
 
 // allow cors
 app.use(function (req, res, next) {
@@ -18,17 +24,27 @@ app.use(function (req, res, next) {
 });
 
 app.get("/update", (req, res) => {
-  execSync("sudo ../scripts/update.sh");
+  execSync("../scripts/update.sh");
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ success: true }));
 });
 
 app.get("/flash", (req, res) => {
   execSync("../scripts/flash.sh");
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ success: true }));
 });
 
-app.get("/alive", (req, res)=>{
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({alive: true}))
-})
+app.get("/text", (req, res) => {
+  arduino.write(`${req.query.text}\n`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ success: true }));
+});
+
+app.get("/alive", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ alive: true }));
+});
 
 app.listen(port, async () => {
   console.log(`listening on port ${port}`);
