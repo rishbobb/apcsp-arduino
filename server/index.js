d9d9e4d4af306ff66be6ffae983b7dcf4d8e3940
Rishabh <61441648+rishbobb@users.noreply.github.com>
2023-10-30 22:34:44 -0700

    comment REST api

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 import express from "express";
 import { execSync } from "child_process";
 import { SerialPort } from "serialport";
-import * as pcm from "pcm";
-import * as dotenv from "dotenv";
 import { createInterface } from "readline";
 import { WebSocketServer } from "ws";
-dotenv.config();
 
+// expressjs rest api
 const app = express();
 const port = 3000;
+// ws server
 const wss = new WebSocketServer({ port: 6969 });
+// holds lines received from arduino serial
 let lines = [];
 
+// initialize serial connection with arduino
 const arduino = new SerialPort({
   path: "/dev/ttyACM0",
   baudRate: 2000000,
 });
 
+// read lines as they come in and push to console and lines array
 var lineReader = createInterface({
   input: arduino,
 });
@@ -26,10 +28,11 @@ lineReader.on("line", function (line) {
   console.log(line);
 });
 
+// send data from ws to arduino
 wss.on("connection", (ws) => {
   ws.on("message", (data) => {
     console.log(`Got direction ${data} from ws`);
-    // arduino.write(`${data}\n`);
+    // send data to arduino
     arduino.write(data);
   });
 });
@@ -45,26 +48,35 @@ app.use(function (req, res, next) {
   next();
 });
 
-// static html
+// static html for frontend
 app.use(express.static("static"));
 
+// endpoint for updating the server
 app.get("/update", (req, res) => {
+  // run update script
   execSync("../scripts/update.sh");
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ success: true }));
 });
 
+// endpoint for compiling and flashing arduino
 app.get("/flash", (req, res) => {
+  // run flash script
   execSync("../scripts/flash.sh");
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ success: true }));
 });
 
+// endpoint for getting sensor data from arduino
 app.get("/data", (req, res) => {
+  // clear lines array
   lines = [];
+  // execute command for arduino to get data and send it
   arduino.write("4");
+  // wait for serial to recieve 3 lines
   let timeoutfunc = () => {
     if (lines.length >= 3) {
+      // once 3 lines receieved, send it in json data to client
       res.setHeader("Content-Type", "application/json");
       res.end(
         JSON.stringify({
@@ -81,25 +93,22 @@ app.get("/data", (req, res) => {
   setTimeout(timeoutfunc, 500);
 });
 
+// endpoint for sending commands to arduino (same as ws)
 app.get("/direction", (req, res) => {
   console.log(`Got direction ${req.query.direction}`);
-  // arduino.write(`${req.query.direction}\n`);
+  // send data to arduino
   arduino.write(req.query.direction);
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ success: true, direction: req.query.direction }));
 });
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
+// just to check if webserver is alive
 app.get("/alive", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ alive: true }));
 });
 
+// Start server
 app.listen(port, async () => {
   console.log(`listening on port ${port}`);
 });
